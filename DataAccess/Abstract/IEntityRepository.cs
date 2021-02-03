9c382c1ea76ea70ea4b1b0921f11d8efeade3d4c
cagataycinar <ccinar.99@gmail.com>
2021-02-03 23:48:24 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+using Entities.Abstract;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint 
+    //calss : referans tip
+    // IEntity : IEntity Olabilir  veya IEntity implemente eden bir nesne olabilir.
+    //new() : new' lenebilir olmalı.
+    public interface IEntityRepository<T> where T:class, IEntity
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        List<T> GetAllByCategory(int categoryId);
+    }
+}
